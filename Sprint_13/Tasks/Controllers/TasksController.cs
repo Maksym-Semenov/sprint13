1cfa1fca7162fac3f236d8b80c3601474e48fb7a
Alina Horkovaya <horkovayalina@gmail.com>
2022-10-31 20:15:49 +0200

    init

---
+++ Sprint_13/Tasks/Controllers/TasksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tasks.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Greeting()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Supermarkets()
+        {
+            return View();
+        }
+        public IActionResult ShoppingList()
+        {
+            return View();
+        }
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
