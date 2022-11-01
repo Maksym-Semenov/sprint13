e76c56afdc206d890a9b028d5b923667bb55e09b
Alina Horkovaya <horkovayalina@gmail.com>
2022-11-01 21:55:58 +0200

    init

--- Sprint_13/Tasks/Controllers/HomeController.cs
+++ Sprint_13/Tasks/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Tasks.Controllers
             return View();
         }
 
-        public IActionResult SprintTasks()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
