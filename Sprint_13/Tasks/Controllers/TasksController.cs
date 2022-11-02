e64d48e048e6bc7d37d443fa35b78e97861e3f9e
Andrii Tsylia <andrii.tsylia@outlook.com>
2022-11-02 14:51:13 +0200

    Added [HttpGet] [HttpPost] ShoppingCart action, ShoppingCart view.

--- Sprint_13/Tasks/Controllers/TasksController.cs
+++ Sprint_13/Tasks/Controllers/TasksController.cs
@@ -16,35 +16,54 @@ namespace Tasks.Controllers
             ["Cola"] = 10
         };
 
+        public IActionResult SprintTasks()
+        {
+            return View();
+        }
+
         public IActionResult Greetings()
         {
             return View();
         }
+
         public IActionResult Products()
         {
             return View();
         }
+
         public IActionResult SuperMarkets()
         {
             ViewBag.Markets = markets;
             return View();
         }
+
         public IActionResult ShoppingList()
         {
             return View(shoppingList);
         }
+
+        [HttpGet]
         public IActionResult ShoppingCart()
         {
+            ViewBag.Markets = markets;
+            ViewBag.ShoppingList = shoppingList;
+            DateTime today = DateTime.Today;
+            ViewBag.Today = new DateTime(today.Year, today.Month, today.Day);
+            ViewBag.Tomorrow = new DateTime(today.Year, today.Month, today.Day).AddDays(1);
+            ViewBag.DayAfterTomorrow = new DateTime(today.Year, today.Month, today.Day).AddDays(2);
+
             return View();
         }
+
+        [HttpPost]
+        public IActionResult ShoppingCart(string fullName, string address)
+        {
+            return Content($"Your product will be shipped at: {address}. Bon appetite, {fullName}!");
+        }
+
         public IActionResult TimeToBuy()
         {
             return PartialView();
-        }
-
-        public IActionResult SprintTasks()
-        {
-            return View();
         }
     }
 }
